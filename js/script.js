9707aac06b26e951d47947e66d641100681f5563
Isengon <alex-kuzetssov@yandex.ru>
2020-12-17 01:23:22 +0300

    .

--- js/script.js
+++ js/script.js
@@ -6,3 +6,38 @@ window.addEventListener('resize', function() {
 
   document.documentElement.style.setProperty('--vh', vh+'px');
 });
+
+
+
+const setIntroBgPosition = () => {
+  var introBlock = document.getElementById('intro')
+
+  if (document.body.clientWidth <= 1030) {
+    introBlock.style.backgroundPosition = `calc(-450px) calc(50% + 150px)`
+  } else if (document.body.clientWidth > 1030) {
+    const containerPaddingLeft = (document.body.clientWidth - 1000) / 2
+    introBlock.style.backgroundPosition = `calc(${containerPaddingLeft}px - 450px) calc(50% + 150px)`
+  }
+}
+setIntroBgPosition()
+
+window.addEventListener("resize", function() {
+  setIntroBgPosition()
+});
+
+
+
+
+let currentString = 1
+
+setInterval(() => {
+  document.getElementById(`stringOption${currentString}`).classList.remove("active");
+
+  if (currentString === 3) {
+    currentString = 1
+  } else {
+    currentString = currentString + 1
+  }
+
+  document.getElementById(`stringOption${currentString}`).classList.add("active");
+}, 4000);
