fafc3c2850aba07e01edd6e4cb97ac2c141708e1
Isengon <alex-kuzetssov@yandex.ru>
2020-12-17 02:50:56 +0300

    bugs fix

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ setInterval(() => {
 
 
 
-var form = document.getElementById('form');
+let form = document.getElementById('form');
   form.addEventListener("submit", e => {
     e.preventDefault();
     fetch(form.action, {
@@ -58,4 +58,4 @@ var form = document.getElementById('form');
       // you can put any JS code here
       window.location.href = 'http://127.0.0.1:5500/#connect';
     });
-  });
+});
